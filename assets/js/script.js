8f14223db5b5849623de1b84ffa7a2a2513d94d0
Ronan Viana <ronanssviana@gmail.com>
2018-02-27 00:05:20 -0300

    find better place to map fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: -22.970722, lng: -43.182365};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 15,
         disableDefaultUI: true,
         center: uluru
     });
